00de139d7e591f071cfe6543f4072fd44ac231fb
weiwei_git <1667187136@qq.com>
2018-12-27 20:51:20 +0800

    再次上传文件

---
+++ JumpText_A/JumpText_ATests/JumpText_ATests.m
@@ -0,0 +1,39 @@
+//
+//  JumpText_ATests.m
+//  JumpText_ATests
+//
+//  Created by Boxz on 2018/5/7.
+//  Copyright © 2018年 zhaoyundianshang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JumpText_ATests : XCTestCase
+
+@end
+
+@implementation JumpText_ATests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
